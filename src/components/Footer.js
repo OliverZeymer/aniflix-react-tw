ca284da73c9763ffc8caea915556981c7432c805
OtakuBoy420 <U28282@rts-365.dk>
2022-08-22 17:25:20 +0200

    pagination and screen width fixed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -20,16 +20,36 @@ const Footer = () => {
   return (
     <footer className="bg-black px-12 w-full bg-primary-background text-primary-text py-12 mt-24 transition-all">
       <div className="flex gap-4">
-        <a className="transition-all hover:scale-125" href="https://twitter.com" target="_blank" rel="noreferrer">
+        <a
+          className="transition-all hover:scale-125"
+          href="https://twitter.com"
+          target="_blank"
+          rel="noreferrer"
+        >
           <BsTwitter />
         </a>
-        <a className="transition-all hover:scale-125" href="https://youtube.com" target="_blank" rel="noreferrer">
+        <a
+          className="transition-all hover:scale-125"
+          href="https://youtube.com"
+          target="_blank"
+          rel="noreferrer"
+        >
           <BsFacebook />
         </a>
-        <a className="transition-all hover:scale-125" href="https://instagram.com" target="_blank" rel="noreferrer">
+        <a
+          className="transition-all hover:scale-125"
+          href="https://instagram.com"
+          target="_blank"
+          rel="noreferrer"
+        >
           <BsInstagram />
         </a>
-        <a className="transition-all hover:scale-125" href="https://github.com" target="_blank" rel="noreferrer">
+        <a
+          className="transition-all hover:scale-125"
+          href="https://github.com"
+          target="_blank"
+          rel="noreferrer"
+        >
           <BsGithub />
         </a>
       </div>
